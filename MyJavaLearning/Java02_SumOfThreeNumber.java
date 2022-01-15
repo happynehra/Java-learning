bc7f7bfc5e4a2264a0211812d2665563481cbf79
Happy Nehra <happynehra777@gmail.com>
2022-01-15 13:07:50 +0530

    sumofhtreenumber added

---
+++ MyJavaLearning/Java02_SumOfThreeNumber.java
@@ -0,0 +1,14 @@
+package MyJavaLearning;
+
+public class Java02_SumOfThreeNumber{
+    public static void main(String[]agrs){
+        int a = 8;
+        int b = 9;
+        int c = 3;
+        int sum = a+b+c;
+        System.out.println(sum);
+    }
+
+    }
+
+    
